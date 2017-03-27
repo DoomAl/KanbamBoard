d071c2e7a1f5e4a20abfeaf740497101511e3a3e
DoomAl <alvaromonferrer11@gmail.com>
2017-03-27 12:50:13 +0100

    Add files via upload

---
+++ Person/Administrator.java
@@ -0,0 +1,13 @@
+package person;
+
+public class Administrator extends Person {
+	
+	public Administrator(){
+		super();
+	}
+	
+	public Administrator(String Name,String Role){
+		super(Name,Role);
+	}
+
+}
---
+++ Person/Developer.java
@@ -0,0 +1,13 @@
+package person;
+
+public class Developer extends Person {
+	
+	public Developer(){
+		super();
+	}
+	
+	public Developer(String Name,String Role){
+		super(Name,Role);
+	}
+	
+}
